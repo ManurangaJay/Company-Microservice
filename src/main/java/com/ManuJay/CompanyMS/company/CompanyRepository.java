e70cc9b3000bf319b4a81e22f26dca221f688e72
ManurangaJay <manurangajayawardhana@gmail.com>
2024-06-02 23:41:42 +0530

    Company Microservice was created

---
+++ src/main/java/com/ManuJay/CompanyMS/company/CompanyRepository.java
@@ -0,0 +1,6 @@
+package com.ManuJay.CompanyMS.company;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CompanyRepository extends JpaRepository<Company, Long> {
+}
